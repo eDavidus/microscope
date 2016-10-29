59fb32544a79a51ba8290e805017773a945121d0
edavidus <ebel_david@yahoo.fr>
2016-10-29 21:05:51 +0000

    comments

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,5 +6,10 @@ Template.postItem.helpers({
         var a = document.createElement('a'); // create an empty anchor html element and store in memory
         a.href = this.url; // set href attribute to the element
         return a.hostname; // hostname property to get back the link's  domain name without the rest of the url
-    }
+    },
+    // commentsCount: function() {
+    //     console.log("from postItem template ");
+    //     console.log(this);
+    //   return Comments.find({postId: this._id}).count();  
+    // }
 });
